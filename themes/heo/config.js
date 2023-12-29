7d569bc399770219f95134a983471e462a3923ac
tshiyuoo <90383151+tshiyuoo@users.noreply.github.com>
2023-12-29 14:41:02 +0800

    update

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   HEO_HOME_BANNER_ENABLE: true,
 
   HEO_SITE_CREATE_TIME: '2023-12-29', // 建站日期，用于计算网站运行的第几天
-``
+
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
     // { title: '欢迎来到我的博客', url: 'https://blog.tshiyuoo' },
